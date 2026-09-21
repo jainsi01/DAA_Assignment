f54a2f378d3a13cf99adcfbc941806b01120272a
jainsi01 <sinhajainsi@gmail.com>
2026-09-21 10:58:31 +0530

    Add files via upload

---
+++ peakelement.java
@@ -0,0 +1,24 @@
+public class peakelement {
+        public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+       peakelement solution = new peakelement();
+        int[] nums = {1, 2, 3, 1};
+        int result = solution.findPeakElement(nums);
+        System.out.println("Peak Element Index: " + result);
+        System.out.println("Peak Element: " + nums[result]);
+    }
+
+}
